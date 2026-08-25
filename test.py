eabf3ea57cc0cde889de8a4971c01c995db81a51
ke <killer123578910@gmail.com>
2026-08-25 20:19:06 +0700

    json

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import json
 
-tags=["greetings","g_info","services_prices","utils","terms_cond","goodbye"]
-data={"ts":[]}
-for t in tags:
-    data["ts"].append({"tag":t})
-with open("intents.json","w") as f:
-    f.write(json.dumps(data))
+with open("intents.json", encoding="utf-8") as f:
+    data = json.load(f)
+
+for intent in data["intents"]:
+    intent["patterns"] = [
+        pattern.split()
+        for pattern in intent["patterns"]
+    ]
+
+with open("intents.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
