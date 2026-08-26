5600a36fccb9d1be736a16c2d1417ea8042b9239
Khuong <killer123578910@gmail.com>
2026-08-26 13:31:22 +0700

    xu li IO

--- choicenrep/inputanas.py
+++ choicenrep/inputanas.py
@@ -1,10 +1,13 @@
 import json
 import re
 import unicodedata
-#stripinput
-user_input=input().lower().strip()
+import random
+data={}
+with open("intents.json",encoding="utf-8") as f:
+    data=json.load(f)
 
-
+def randome(rep):
+    return random.choice(rep)
 #nomalize texts and remove sign by unicode
 #working like a map, eX: 17 denominations of A will be translate into A, while Đ just need to turn into D, same will lowercase
 BANG_XOA_DAU = str.maketrans(
@@ -16,9 +19,37 @@ def xoa_dau(txt: str) -> str:
     if not unicodedata.is_normalized("NFC", txt):
         txt = unicodedata.normalize("NFC", txt)
     return txt.translate(BANG_XOA_DAU)
-user=xoa_dau(user_input)
 
-#split by regex
-user=re.split("\W+",user)
-print(user)
 #chunk comparing: compare word by word for each tag and calculate the intersection,the tag with the highest inter will be chosen
+#when overlap,the first encounter(priortized by position in json) will be chosen
+def takingtag(data,user):   
+    tag_point={}
+    for intent in data["intents"]:
+        max_point=0
+        for k in data["intents"][intent]["patterns"]:
+            cur_point=len(set(k)&set(user))
+            if max_point<cur_point:
+                max_point=cur_point 
+        tag_point[intent]=max_point
+    tagvalue=max(tag_point.values())
+    tag=max(tag_point,key=tag_point.get)
+    return "fallback" if tagvalue==0 else tag
+
+
+
+if __name__=="__main__":
+    #stripinput
+    user_input=input().lower().strip()
+    user=xoa_dau(user_input)
+
+    #split by regex
+    user=re.split("\W+",user)
+    print(user)
+    
+   
+    intents=data['intents']
+    response=randome(intents[takingtag(data,user)]["responses"])
+    print(response)
+    f.close()
+            
+        
--- test.py
+++ test.py
@@ -2,12 +2,5 @@ import json
 
 with open("intents.json", encoding="utf-8") as f:
     data = json.load(f)
-
-for intent in data["intents"]:
-    intent["patterns"] = [
-        pattern.split()
-        for pattern in intent["patterns"]
-    ]
-
-with open("intents.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+    for intents in data["intents"]:
+        print(intents["patterns"])
